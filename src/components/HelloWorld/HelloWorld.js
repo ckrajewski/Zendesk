ba3a57d419bda52d8d10332f09be9924ab16f760
chris <chris@chriss-MacBook-Air.local>
2019-02-19 23:33:52 -0800

    adding classnames

--- src/components/HelloWorld/HelloWorld.js
+++ src/components/HelloWorld/HelloWorld.js
@@ -2,12 +2,19 @@ import React from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from 'redux';
 import { fetchStuff } from "../../actions/action";
-import "./HelloWorld.css";
+import classNames from 'classnames/bind';
+import styles from "./HelloWorld.css";
+
+let cx = classNames.bind(styles);
+
 class HelloWorld extends React.Component {
 
   render() {
+    let className = cx({
+      notAnUglyBackground:true,
+    });
   	return (
-    <div className="notAnUglyBackground">
+    <div className={className}>
     Hello World!
      </div> 
     );
