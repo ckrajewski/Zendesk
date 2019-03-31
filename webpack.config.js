72209b0e6f034fc2b126e4bfa44b3e258073302e
chris <chris@chriss-MacBook-Air.local>
2019-03-30 22:30:28 -0700

    adding eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,49 @@
 /*
 alias npm-exec='PATH=$(npm bin):$PATH'
 */
-const  webpack = require('webpack');
-const  path = require('path');
+const webpack = require('webpack');
+const path = require('path');
 const openBrowser = require('react-dev-utils/openBrowser');
 
 module.exports = {
-  context: path.join(__dirname, "src"),
-  devtool: "#eval-sourcemap",
-  entry: "./main.js",
-   module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ["babel-preset-env", "react"],
-          plugins: ["transform-object-rest-spread", "transform-class-properties","babel-plugin-react-css-modules"],
-        },
-      },
-      {
-        test: /\.css$/,
-        loader: "style-loader!css-loader"
-      },
-      {
-        test: /\.sass$/,
-         use: [
-          { loader: "sass-loader" },
-          { loader: "css-loader" },
-          {loader: "postcss"}
+    context: path.join(__dirname, "src"),
+    devtool: "#eval-sourcemap",
+    entry: "./main.js",
+    module: {
+        rules: [{
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                use: [{
+                        loader: "babel-loader",
+                        options: {
+                            presets: ["babel-preset-env", "react"],
+                            plugins: ["transform-object-rest-spread", "transform-class-properties", "babel-plugin-react-css-modules"],
+                        },
+                    },
+                    { loader: "eslint-loader" }
+                ],
+            },
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader"
+            },
+            {
+                test: /\.sass$/,
+                use: [
+                    { loader: "sass-loader" },
+                    { loader: "css-loader" },
+                    { loader: "postcss" }
+                ]
+            }
         ]
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  output: {
-    path: __dirname + "/src/",
-    filename: "mainCreated.js"
-  },
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    output: {
+        path: __dirname + "/src/",
+        filename: "mainCreated.js"
+    },
 };
 if (openBrowser('http://localhost:8080')) {
     console.log('The browser tab has been opened!');
