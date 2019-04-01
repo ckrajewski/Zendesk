f67e283aece5d428ac60a957a5cff4d20eae8b5d
chris <chris@chriss-MacBook-Air.local>
2019-03-31 17:44:03 -0700

    commenting out cx style

--- src/components/HelloWorld/HelloWorld.js
+++ src/components/HelloWorld/HelloWorld.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import classNames from 'classnames/bind';
+// import classNames from 'classnames/bind';
 import { fetchStuff } from '../../actions/action';
 import styles from './HelloWorld.css';
 
-const cx = classNames.bind(styles);
+// const cx = classNames.bind(styles);
 
 class HelloWorld extends React.Component {
   render() {
+    /*
     const className = cx({
       notAnUglyBackground: true,
     });
+    */
     return (
-      <div className={className}>
+      <div className={styles.notAnUglyBackground}>
     Hello World!
       </div>
     );
